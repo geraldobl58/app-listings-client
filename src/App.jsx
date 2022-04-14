f42fc7ce41ab0be17efbebe0d132dee1e8a0f3e2
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-14 13:39:06 -0300

    :sparkles: Structuring Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+
 import Routes from 'routes'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
+      <Footer />
     </BrowserRouter>
   )
 }
