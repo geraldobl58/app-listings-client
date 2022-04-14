8ef1325131cf4cb74aa3168fa00274188c98a874
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-14 13:12:29 -0300

    :tada: first commit

---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+`
+
+export default GlobalStyles
