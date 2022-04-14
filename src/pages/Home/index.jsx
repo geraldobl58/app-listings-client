980b3e8452f46a163443c6f570a430b2f2fbc6ab
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-14 19:06:03 -0300

    :sparkles: Creating search component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,29 @@
-import { Link } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
 
-const Home = () => (
-  <>
-    <h1>Homepage</h1>
-    <Link to="/about">Sobre</Link>
-  </>
-)
+import useApi from 'services/api'
+
+import * as S from './styles'
+
+const Home = () => {
+  const api = useApi()
+
+  return (
+    <S.Container>
+      <S.Content>
+        <S.Search>
+          <form method="GET" action="/ads">
+            <input type="text" name="q" placeholder="O que você procura?" />
+            <select>
+              <option>Selecione</option>
+            </select>
+            <button>Pesquisar</button>
+          </form>
+        </S.Search>
+        <S.Categories>Categories</S.Categories>
+      </S.Content>
+    </S.Container>
+  )
+}
 
 export default Home
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const Container = styled.section``
+
+export const Content = styled.div`
+  width: 1000px;
+  margin: 50px auto;
+`
+
+export const Search = styled.div`
+  color: #fff;
+  padding: 10px;
+  border-radius: 3px;
+  background-color: #157d8b;
+  margin-bottom: 50px;
+
+  input {
+    width: 73%;
+    border: none;
+    box-shadow: 0px 0px 3px #ccc;
+    padding: 15px;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    margin-right: 15px;
+    margin-left: 15px;
+
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  select {
+    width: 10%;
+    border: none;
+    box-shadow: 0px 0px 3px #ccc;
+    padding: 15px;
+    margin-top: 15px;
+    margin-bottom: 15px;
+
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  button {
+    margin-left: 15px;
+    border: none;
+    padding: 18px;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 12px;
+    text-transform: uppercase;
+
+    font-family: 'Montserrat', sans-serif;
+  }
+`
+
+export const Categories = styled.div``
