984b1278afe1563c14a215ca59bc0f0010fbbed5
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-14 14:25:18 -0300

    :sparkles: Creating page not found

---
+++ src/pages/Notfound/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+const Notfound = () => (
+  <>
+    <h1>404</h1>
+    <Link to="/">Home</Link>
+  </>
+)
+
+export default Notfound
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,14 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Notfound from '../pages/Notfound'
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="**" component={Notfound} />
     </Switch>
   )
 }
